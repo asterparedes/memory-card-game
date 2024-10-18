1dfc86835e6bec5a045d16c8c1cc1e16046c4a89
aster <asterparedes0912@gmail.com>
2024-10-17 22:54:15 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// TODO pokemon memory card game
+
+const messageNode = document.createElement("h1");
+messageNode.textContent = "Welcome to Pokemon Memory Card Game";
+
+document.getElementById("root").appendChild(messageNode);
